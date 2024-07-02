b3accdac6ce5efa9488b810a3b0ad0d20fe8ba6f
ewi-orion <brpmdr@mailfence.com>
2024-07-02 11:55:02 -0400

    export switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 // App.js
 import React, { useEffect, useState } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Switch } from 'react-router-dom';
 //import VacationsDreams from './VacationsDreams/VacationsDreams';
 import ShowVacations from './components/ShowVacations';
+import { Switch } from '@mui/material';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<ShowVacations></ShowVacations>}></Route>
-      </Routes>
+      <Switch>
+        <Routes>
+          <Route path='/' element={<ShowVacations></ShowVacations>}></Route>
+        </Routes>
+      </Switch>
     </BrowserRouter>
   );
 }
